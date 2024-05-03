0cab0b02b0da5d06f9c7335084b3a21ea07f32d1
lgong2000 <lgong2000@gmail.com>
2024-05-03 11:06:27 -0500

    JdbcUserDetailsManager and h2-console

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ RestController
 SecurityFilterChain
 formLogin
 InMemoryUserDetailsManager
+
+## Version 2
+InMemoryUserDetailsManager - interface UserDetailsManager - JdbcUserDetailsManager - JdbcDaoImpl
+    - public static final String DEFAULT_USER_SCHEMA_DDL_LOCATION = "org/springframework/security/core/userdetails/jdbc/users.ddl";
+users.ddl - select users.dll(text) - Ctrl+Shift+N - Enter - See the create scripts
+pom.xml
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-data-jdbc</artifactId>
+    </dependency>
+    <dependency>    
+        <groupId>com.h2database</groupId>
+        <artifactId>h2</artifactId>
+        <scope>runtime</scope>
+    </dependency>
+application.properties
+    spring.datasource.name=dashboard
+    spring.datasource.generate-unique-name=false
+    spring.h2.console.enabled=true
+
+H2
+http://localhost:8080/h2-console
+JDBC URL: jdbc:h2:mem:dashboard
+EmbeddedDatabase - run script JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION - Create Tables to H2 Database
+Add user
+PasswordEncoder - Bcrypt
--- src/main/java/com/example/springauthjdbc/config/SecurityConfig.java
+++ src/main/java/com/example/springauthjdbc/config/SecurityConfig.java
@@ -2,12 +2,22 @@ package com.example.springauthjdbc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,21 +26,52 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
     @Bean
-    InMemoryUserDetailsManager users() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("test0001")
-                        .password("{noop}password")
-                        .roles("ADMIN")
-                        .build()
-        );
+    EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("dashboard")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
+    @Bean
+    JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        System.out.println(admin.getPassword());
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+    }
+
+//    @Bean
+//    InMemoryUserDetailsManager users() {
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("test0001")
+//                        .password("{noop}password")
+//                        .roles("ADMIN")
+//                        .build()
+//        );
+//    }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"))
                 .authorizeHttpRequests(auth -> auth
+                        .antMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .headers(headers -> headers.frameOptions().sameOrigin())    // For h2-console
                 .formLogin(withDefaults())
                 .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
