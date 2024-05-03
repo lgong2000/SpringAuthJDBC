57e577c78818e9caf3734a767c41cf6ffa302582
lgong2000 <lgong2000@gmail.com>
2024-05-02 22:19:13 -0500

    Initial Commit

---
+++ src/main/java/com/example/springauthjdbc/SpringAuthJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springauthjdbc/SpringAuthJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
