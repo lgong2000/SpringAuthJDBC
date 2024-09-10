8d5a2aa69ddd297caf2206aad3aad59308a5e5b0
lgong2000 <lgong2000@gmail.com>
2024-09-10 11:30:24 -0500

    Add create user page

---
+++ src/main/resources/static/js/CreateAccount.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+    $("#create").click(function() {
+        let username = $("#username").val();
+        let password = $("#password").val();
+        let cpassword = $("#cpassword").val();
+
+        if (username == '' || password == '' || cpassword == '') {
+            alert("Please fill all the fields!");
+        } else if ((password.length) < 8) {
+            alert("Password should be at least 8 character!");
+        } else if (!(password).match(cpassword)) {
+            alert("Your passwords don't match!");
+        } else {
+            let data = {};
+            data['username'] = username;
+            data['password'] = password;
+
+            $.ajax({
+                url: 'user',
+                type: 'post',
+                data: JSON.stringify(data),
+                contentType: 'application/json; charset=utf-8',
+                success: function(response){
+                    window.location.href = '/'
+                    //$("form")[0].reset();
+                },
+            });
+        }
+    });
+});
